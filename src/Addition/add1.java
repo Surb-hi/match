f1a7e21d5aa8bd3b003189ac5e2920965b35c1d6
surb-hi <surbhikakadiya1005@gmail.com>
2022-03-11 10:05:55 +0000

    cucumber

---
+++ src/Addition/add1.java
@@ -0,0 +1,16 @@
+package Addition;
+
+public class add1 {
+
+    public static void main (String[] args){
+
+        int a= 5;
+        int b= 6;
+        int c= 4;
+
+        System.out.println(a+b+c);
+        System.out.println(a-b/c);
+        System.out.println(b+c-a);
+
+    }
+}
